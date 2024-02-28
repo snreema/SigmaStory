7101815c4aa496a319a3e9a53e32369984782a31
Nessie Reema S <103185396+snreema@users.noreply.github.com>
2024-02-27 19:20:20 -0500

    Committed Assets and main page
    Created landing page, added assets.

--- SigmaStory/ViewController.swift
+++ SigmaStory/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "storyBackground")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
+        
+        let overlay = UIView(frame: self.view.bounds)
+        overlay.backgroundColor = UIColor.white.withAlphaComponent(0.6)
+        self.view.addSubview(overlay)
+        self.view.insertSubview(overlay, aboveSubview: backgroundImage)
+    
     }
-
-
+    
 }
 
